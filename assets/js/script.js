21723a95d97125114e9c5dfea3f267b54d6c1f05
ALYSSA_MAPLES <armaples@ucsc.edu>
2022-11-13 13:02:19 -0500

    FIXED, now records value for each individual hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ var footerEl = $("<footer>");
 var dateSection = $("<section>");
 var scheduleSection = $("<section>");
 var title = $("<h1>");
-var day = $("<h2>")
+var day = $("<h2>");
 var date = $("<h3>");
-var footerText = $("<h4>")
+var footerText = $("<h4>");
 var headingFont = "italic bold max(5vw, 35px) collector-comic, Comic Sans MS, cursive";
 var bodyFont = "max(2vw, 15px) grenadine-MVB, Baskerville, serif";
 var orange = "#F49D36";
@@ -20,9 +20,8 @@ var garfield = $("<img>").attr("src", "./assets/images/garfield.png").css("max-w
 var eventBox;
 var timeBlock;
 var present = dayjs().hour();
-console.log(present);
-// create container for timeblocks
-// create for-loop that generates timeblocks 8am-6pm
+var monthDay = dayjs().format("MM" + "/" + "DD");
+console.log("Current Hour: " + present);
 
 // Append
 headerEl.appendTo(bodyEl);
@@ -34,7 +33,6 @@ dateSection.appendTo(mainEl);
 day.appendTo(dateSection);
 date.appendTo(dateSection);
 scheduleSection.appendTo(mainEl);
-// timeBlock.appendTo(scheduleSection);
 
 footerEl.appendTo(bodyEl);
 footerText.appendTo(footerEl);
@@ -44,7 +42,6 @@ title.text("Workday Scheduler");
 day.text(dayjs().format("dddd"))
 date.text(dayjs().format("MMM" + ". " + "DD" + ", " + "YYYY"));
 footerText.text("Copyright © 2022 Alyssa Maples")
-// timeBlock.text("TestforBlock");
 
 // Styling
 bodyEl.css("display", "flex").css("flex", "1 1 90vw").css("flex-flow", "column wrap").css("justify-content", "center").css("align-items", "center").css("text-align", "center");
@@ -61,18 +58,21 @@ footerText.css("color", orange).css("font", headingFont).css("font-size", "max(1
 // Arrays
 var hours = [];
 
+// Functions
 $(document).ready(function() {
     // Creates array items
     for (let i = 8; i < 19; i++) {
         hours.push(i);
     };
 
+    // Creates containers + boxes for each item in array
     hours.forEach((element) => {
         var timeBlock = $("<div>").appendTo(scheduleSection).css("width", "100vw").css("height", "100%").css("display", "flex").css("flex-flow", "row no-wrap").css("justify-content", "center").css("align-items", "center").css("align-content", "center");
 
         var hourBox = $("<section>").appendTo(timeBlock).text(dayjs().hour(element).format("h" + "a")).css("flex-grow", "1").css("width", "25vw").css("padding", "5px").css("height", "100%").css("color", orange);
 
-        var eventBox = $("<section>").appendTo(timeBlock).css("width", "50vw").css("flex-grow", "2").css("padding", "5px").css("height", "100%").css("color", "white").attr("contenteditable", "true").css("outline", "0px").css("overflow-y", "hidden").css("border", "1px solid" + orange);
+        var eventBox = $("<section>").appendTo(timeBlock).css("width", "50vw").css("flex-grow", "2").css("padding", "5px").css("height", "100%").css("color", "white").attr("contenteditable", "true").css("outline", "0px").css("overflow-y", "hidden").css("border", "1px solid" + orange).attr("id", element);
+        console.log(eventBox.attr("id"));
 
         var saveBox = $("<section>").appendTo(timeBlock).css("width", "25vw").css("padding", "5px").css("height", "100%").css("color", orange);
 
@@ -84,36 +84,32 @@ $(document).ready(function() {
             };
         });
 
+        // Determines background color of event 
         var thisTime = parseInt(element);
-
         if (thisTime < present) {
             eventBox.css("background-color", lightBlue);
+            hourBox.css("color", lightOrange);
         } else if (thisTime === present) {
             eventBox.css("background-color", orange).css("color", blue).css("border-color", blue);
+            hourBox.css("color", blue);
         } else {
             eventBox.css("background-color", lightOrange);
         }
 
+        // Save Button Event Listener
         saveButton.click(function() {
-            var content = document.querySelector("[contenteditable]");
-            var text = content.textContent;
-            localStorage.setItem("toDo " + element, text);
+            var content = $("#"+element);
+            var text = content.text();
+            localStorage.setItem("toDo " + monthDay + " @ " + element, text);
             console.log("text for hour " + element + ": " + text);
-        });
-
+            });
+    
+        //Looks for previous values in local storage
+        eventBox.text(localStorage.getItem("toDo " + monthDay + " @ " + element));
         console.log("Hour " + element + " added.");
     });
-
-    console.log("Current hours: " + hours);
+    console.log("Hours in schedule: " + hours);
 });
-
-
-    // Save Button
-
-
-
-
-// Functions
 
 // Event Listeners
 
